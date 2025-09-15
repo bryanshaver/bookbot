6eaa55d187c81e7f394d9ce149e7e5329bf6ced1
Bryan Shaver <bshave@yahoo.com>
2025-09-15 12:25:43 -0400

    create bookbot to track character count of varying books

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, character_count, sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +7,20 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    book = get_book_text("../bookbot/books/frankenstein.txt")
-    get_num_words(book)
-
+    print(sys.argv)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    book = get_book_text(filepath)
+    word_count = get_num_words(book)
+    char_dict = character_count(book)
+    sorted_count = sort_dict(char_dict)
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(word_count)
+    print("--------- Character Count -------")
+    for e in sorted_count:
+        print(f"{e["char"]}: {e["num"]}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -3,5 +3,29 @@ def get_num_words(text):
     count = 0
     for w in words:
         count += 1
-    print(f"{count} words found in the document")
+    return f"Found {count} total words"
 
+def character_count(text):
+    lower_text = text.lower()
+    character_dict = {}
+    text_split = list(lower_text)
+    for c in text_split:
+        if c in character_dict:
+            character_dict[c] += 1
+        else:
+            character_dict.update({c: 1})
+    
+    return character_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(counts):
+    records = []
+    for ch, cnt in counts.items():
+        if ch.isalpha():
+            records.append({"char": ch, "num": cnt})
+        else:
+            pass
+    records.sort(reverse=True, key=sort_on)
+    return records
